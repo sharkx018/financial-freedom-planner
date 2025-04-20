afb43b6b2ff744fd2f3eed3361012a1bb900f0b5
mukul_verma <aryanv018@gmail.com>
2025-04-21 04:05:33 +0530

    Added the sip required calculator

--- internal/helper/calculator.go
+++ internal/helper/calculator.go
@@ -7,3 +7,22 @@ func InflationCalculator(amount float64, time int64, rate float64) float64 {
 	futureValue := amount * math.Pow(1+(rate/100), float64(time))
 	return RoundToDecimals(futureValue, 2)
 }
+func CalculateSIPRequired(targetAmount float64, years int, annualGrowthRate float64, stepUpPercentage float64) float64 {
+	monthlyRate := annualGrowthRate / (12 * 100)
+	totalMonths := years * 12
+
+	denominator := 0.0
+
+	for i := 0; i < totalMonths; i++ {
+		currentYear := i / 12
+		stepUpMultiplier := math.Pow(1+stepUpPercentage/100, float64(currentYear))
+		monthsLeft := totalMonths - i
+
+		installmentFV := stepUpMultiplier * math.Pow(1+monthlyRate, float64(monthsLeft))
+		denominator += installmentFV
+	}
+
+	sipAmount := targetAmount / denominator
+
+	return RoundToDecimals(sipAmount, 2)
+}
